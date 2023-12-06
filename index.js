fb44c7ef1822892406612fe41015fdd2adf17ca0
Abhishek Prasad <aprasad7@csus.edu>
2023-12-06 01:59:13 -0800

    modified files to make a Stopwatch Website

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
-const myDiv = document.getElementById("myDiv");
-window.addEventListener("keydown", move);
-let x = 0;
-let y = 0;
+const timeDisplay = document.querySelector("#timeDisplay");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
 
-function move(event){
-    
-    switch(event.key){
-        case "ArrowDown":
-            y+=5;
-            myDiv.style.top = y + "px";
-            break;
-        case "ArrowUp":
-            y-=5;
-            myDiv.style.top = y + "px";
-            break;
-        case "ArrowRight":
-            x+=5;
-            myDiv.style.left = x + "px";
-            break;
-        case "ArrowLeft":
-            x-=5;
-            myDiv.style.left = x + "px";
-            break;
-        default:
-            break;
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalId;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
+startBtn.addEventListener("click", () => {
+    if(paused){
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime, 1000);
+    }
+});
+pauseBtn.addEventListener("click", () => {
+    if(!paused){
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+});
+resetBtn.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00:00:00";
+});
+
+function updateTime(){
+    elapsedTime = Date.now() - startTime;
+
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+    function pad(unit){
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
     }
 }
